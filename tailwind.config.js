0ce063651ed100aa3ed0f2bf6a1e808bf0a4369e
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-04-30 14:04:10 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // screens: {
+    //   'sm': '375px',
+    //   'md': '768px',
+    //   'lg': '1280px',
+    // }
+    colors : {
+      'bright-yellow': '#fbf8f3',
+      'light-blue': '#1da1f2',
+      'light-gray': '#737373',
+      'bright-cyan': '#d7f5f5',
+      'logo-color': '#3b86f5',
+      'white': '#ffffff',
+      'dark-cyan': '#2e819f',
+      'dark-yellow': '#fdcb77',
+
+    }
+    ,
+
+    extend: {},
+  },
+  plugins: [],
+}
